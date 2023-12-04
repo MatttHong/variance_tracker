ee191f7d127c10965296c58c647c98f8a9a67098
MatttHong <98180121+MatttHong@users.noreply.github.com>
2023-12-03 19:26:38 -0600

    asdf

--- .github/GetPostFunction/main.py
+++ .github/GetPostFunction/main.py
@@ -1,27 +1,4 @@
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        if req.method == "GET":
-            # Handle GET request
-            message = "This is a GET request."
-        elif req.method == "POST":
-            # Handle POST request
-            req_body = req.get_json()
-            message = f"This is a POST request with data: {req_body}"
-        else:
-            return func.HttpResponse(
-                "Method not allowed",
-                status_code=405
-            )
-
-        return func.HttpResponse(
-            message,
-            mimetype="text/plain",
-            status_code=200
-        )
-    except Exception as e:
-        return func.HttpResponse(
-            "Internal Server Error",
-            status_code=500
-        )
+    return func.HttpResponse("Hello, World!")
